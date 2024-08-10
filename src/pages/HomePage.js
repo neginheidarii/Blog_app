339b571da19bd7d9b543c9a021cc72419c528ef1
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-08-10 17:06:10 -0400

    Created home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,7 @@ const HomePage = () => {
   return (
     <>
       <h1>Hello, welcome to my blog!</h1>
-      <p>
-        Welcome to my blog! Proin congue ligula id risus posuere, vel eleifend
-        ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt
-        egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In
-        tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non
-        fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus
-        euismod.
-      </p>
+      
       <p>
         Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices
         dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu.
